2817a7faad43536c86de540a5a18748392efc3f5
Kamolphan Lewprasert <kamolphan.fony@gmail.com>
2024-06-22 09:37:45 +0000

    add: workshop4 files

--- dags/workshop4.py
+++ dags/workshop4.py
@@ -1,6 +1,5 @@
 from airflow.models import DAG
 from airflow.decorators import dag, task
-from airflow.operators.python import PythonOperator
 from airflow.providers.mysql.hooks.mysql import MySqlHook
 from airflow.utils.dates import days_ago
 import pandas as pd
